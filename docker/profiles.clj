3f5d86cc9a9d945dd2d2f02d966dd2ef3626a26a
Ian McEwen <mian@cyverse.org>
2021-10-08 15:43:53 -0700

    Update docker/profiles.clj

--- docker/profiles.clj
+++ docker/profiles.clj
@@ -1,4 +1,1 @@
-{:auth {:repository-auth
-        {#"https://clojars.org/repo" {:username :env :password :env}}
-        {#"https://repo.clojars.org" {:username :env :password :env}}
-        {"clojars" {:username :env :password :env}}}}
+{:auth {:repository-auth {#"https://repo.clojars.org" {:username :env :password :env}}}}
