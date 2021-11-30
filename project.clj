3092b78a4884c31b6a999719c32d1bbfeb0730b4
Ian McEwen <mian@cyverse.org>
2021-11-30 16:12:11 -0700

    Version 3.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clj-jargon "3.0.1"
+(defproject org.cyverse/clj-jargon "3.0.2-SNAPSHOT"
   :description "Clojure API on top of iRODS's jargon-core."
   :url "https://github.com/cyverse-de/clj-jargon"
   :license {:name "BSD"
