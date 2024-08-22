56649177f1a3f4bc50648a9b63fc70967dd6dd67
Sarah Roberts <sonoran.sarah@gmail.com>
2024-08-21 17:16:22 -0700

    CORE-2006: updated dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Clojure API on top of iRODS's jargon-core."
   :url "https://github.com/cyverse-de/clj-jargon"
   :license {:name "BSD"
-            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :plugins [[jonase/eastwood "1.4.3"]
             [lein-ancient "0.7.0"]
             [test2junit "1.4.4"]]
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.clojure/tools.logging "1.3.0"]
                  [org.irods.jargon/jargon-core "4.2.2.1-RELEASE"
                   :exclusions [[org.jglobus/JGlobus-Core]
@@ -24,7 +24,7 @@
                  [medley "1.4.0"]
                  [slingshot "0.12.2"]
                  [org.cyverse/otel "0.2.6"]
-                 [org.cyverse/clojure-commons "3.0.8"]]
+                 [org.cyverse/clojure-commons "3.0.9"]]
   :profiles {:repl {:source-paths ["repl"]}}
   :eastwood {:exclude-linters [:unlimited-use :non-dynamic-earmuffs]}
   :repositories [["cyverse-de"
